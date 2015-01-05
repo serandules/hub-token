9f7c9048bb8535c3154cadbce14f523832e56367
ruchiraw <ruchira.wageesha@gmail.com>
2015-01-06 01:56:38 +0530

    updating

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var debug = require('debug')('serandules-hub-token');
+var debug = require('debug')('serandules:hub-token');
 module.exports = function (token, callback) {
     callback(false, {
         id: token
